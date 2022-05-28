704edb68bb8d638b7025f9b76fd2cd70d32d2f8b
Víctor García <95133973+v1cGM@users.noreply.github.com>
2022-05-28 20:14:53 +0200

    Update autoupdate.sh

--- autoupdate.sh
+++ autoupdate.sh
@@ -1,7 +1,7 @@
 # Author: Víctor García (aka v1c_gm)
 
 function autoupdate(){
-	sudo pacman -Syu archlinux-keyring; paru -Syua; systemctl daemon-reload
+	sudo pacman -Syu archlinux-keyring;
 	echo "\n\n\t# UPDATING GITHUB REPOSITORIES #"
 	for e in ~/Desktop/$USER/repos/*; do echo "\n" && cd $e && pwd && git pull .; done; cd; searchsploit -u
 	echo "\n\n\t# UPDATING INTERNAL DATABASE #"
