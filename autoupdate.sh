0ff9052edf882bfaf7fbec3285231b0a0cf460f6
Víctor García <95133973+v1xoo@users.noreply.github.com>
2022-07-24 02:26:07 +0200

    Update autoupdate.sh

--- autoupdate.sh
+++ autoupdate.sh
@@ -4,9 +4,7 @@
 
 function autoupdate(){
 	sudo pacman -Syu archlinux-keyring
-	echo "\n\n\t# UPDATING GITHUB REPOSITORIES #"
-	for e in ~/Desktop/$USER/repos/*; do echo "\n" && cd $e && pwd && git pull .; done; cd; searchsploit -u
-	echo "\n\n\t# UPDATING INTERNAL DATABASE #"
-	sudo updatedb
+	echo "\n\n\t# UPDATING REPOSITORIES AND INTERNAL DATABASE #"
+	searchsploit -u; sudo updatedb
 	echo "\n\n\t# AUTOUPDATE SUCCESSFULLY COMPLETED #"
 }
